7e5736d378bc6fa8d968bdcdccce557b24f80160
Jamaal Henry <jamaalhenry30@gmail.com>
2022-03-06 14:03:04 -0500

    edited login route method to check user password for login implementations

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
-
+from werkzeug.security import check_password_hash
 
 ###
 # Routing for your application.
@@ -34,17 +34,23 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
             # Get the username and password values from the form.
+            username = form.username.data
+            password = form.password.data
+            user = UserProfile.query.filter_by(username=username,password=password).first()
 
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
+            if check_password_hash(user.password,password):
+
 
             # get user id, load into session
-            login_user(user)
+                login_user(user)
+                return redirect(url_for('secure_page'))
 
             # remember to flash a message to the user
             return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
